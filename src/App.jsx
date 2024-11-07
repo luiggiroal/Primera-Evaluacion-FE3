1b50c493602972cff2302a0d066ea1d53244aa60
luiggiroal <24428203+luiggiroal@users.noreply.github.com>
2024-11-06 20:05:05 -0500

    Template configurado

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Cargar de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = ({ children }) => {
+  return (
+    <>
+      <div>Hola, soy una tarjeta</div>
+    </>
+  )
+}
+
+export default Card
